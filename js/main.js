25da7400f002eb60e29072d6089894db0e3e72d1
Илья <106013347+Bataevk@users.noreply.github.com>
2023-12-10 15:15:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ e_icon = document.querySelector(".icon");
 alert(tg.initDataUnsafe.user.username);
 
 
-e_username.inner_HTML =  tg.initDataUnsafe.user.username;
+e_username.innerHTML =  tg.initDataUnsafe.user.username;
 if (typeof tg.initDataUnsafe.user.photo_url != undefined ){
     e_icon.src = tg.initDataUnsafe.user.photo_url;
 }
